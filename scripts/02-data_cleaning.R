97f1559207d6ab382e63e634d8386b894ee78c99
Sirui Tan <sirui.tan@utoronto.ca>
2024-02-12 15:40:53 +0000

    update"test_data.R"

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -10,20 +10,20 @@
 #### Workspace setup ####
 library(tidyverse)
 
-#### Clean data ####
+
+# Read in the raw data from the CSV file
 raw_data <- read_csv("inputs/data/country_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(title, prison_population_rate) |>
-  filter(title%in% c ("United States of America","Croatia","United Kingdom: England & Wales","Spain",
-                         "France","Austria", "Italy","Belgium","Switzerland",
-                      "Ireland", "Germany" ,"Denmark" ,"Norway","Netherlands", "Sweden",
-                      "Finland","Egypt","Canada","Norway","Singapore","Japan","Iceland"))|>
-rename(country=title)
+# Clean the data
+cleaned_data <- 
+  raw_data |>  # Pipe operator for cleaner syntax
+  janitor::clean_names() |>  # Clean column names
+  select(title, prison_population_rate) |>  # Select only relevant columns
+  filter(title %in% c ("United States of America","Croatia","United Kingdom: England & Wales","Spain",
+                       "France","Austria", "Italy","Belgium","Switzerland",
+                       "Ireland", "Germany" ,"Denmark" ,"Norway","Netherlands", "Sweden",
+                       "Finland","Egypt","Canada","Norway","Singapore","Japan","Iceland")) |>  # Filter specific countries
+  rename(country = title)  # Rename 'title' column to 'country'
 
- 
-
-#### Save data ####
+# Save the cleaned data to a new CSV file
 write_csv(cleaned_data, "outputs/data/clean_country_data.csv")
